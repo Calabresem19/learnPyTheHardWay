3dbe6a714d0ea7fcd5d919b44d5bbd1b545b9481
mike <mikeclbrs@gmail.com>
2017-01-03 02:02:10 -0500

    commit

---
+++ ex19.py
@@ -0,0 +1,15 @@
+def cheese_andcrackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party!"
+    print "get a blanket. \n"
+
+print "We can just give the function numb ers directly:"
+cheese_and_crackers(20,30)
+
+
+print "OR, we can use variable from out script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
