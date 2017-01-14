37f9a672c5b14a425696bc4883b765074a769ccb
mike <mikeclbrs@gmail.com>
2017-01-13 21:32:07 -0500

    fixed script for 41

--- ex.41-2.py
+++ ex.41-2.py
@@ -2,7 +2,7 @@ import random
 from urllib import urlopen
 import sys
 
-WORD_URL = "http://learncodethehardway.org/words.text"
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
@@ -27,22 +27,22 @@ else:
     PHRASE_FIRST = False
 
 # load up the words from the website
-    for word in urlopen(WORD_URL).readlines():
-        WORDS.append(word.strip())
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(word.strip())
 
-    def convert(snippet, phrase):
-        class_names = [w.capitalize() for w in
-                        random.sample(WORDS, snippet.count("%%%"))]
-        other_names = random.sample(WORDS, snippet.count("***"))
-        results = []
-        param_names = []
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in
+        random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
 
-        for i in range(0, snippet.count('@@@')):
-            param_count = random.randint(1,3)
-            param_names.append(', '.join(random.sample(WORDS, param_count)))
+    for i in range(0, snippet.count('@@@')):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
 
-        for sentence in snippet, phrase:
-            result = sentence[:]
+    for sentence in snippet, phrase:
+        result = sentence[:]
 
         #fake class names
         for word in class_names:
@@ -59,24 +59,24 @@ else:
 
         results.append(result)
 
-        return results
+    return results
 
 
  # keep going untill they hit CTRL-D
-    try:
-        while True:
-            snippets = PHRASES.keys()
-            random.shuffle(snippets)
+try:
+    while True:
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)
 
-            for snippet in snippets:
-                phrase = PHRASES[snippet]
-                question, answer = convert(snippet, phrase)
-                if PHRASE_FIRST:
-                    question, answer = answer, question
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
 
-                print question
+            print question
 
-                raw_input("> ")
-                print "ANSWER: %s\n\n" % answer
-    except EOFError:
-        print "\nBye"
+            raw_input("> ")
+            print "ANSWER: %s\n\n" % answer
+except EOFError:
+    print "\nBye"
